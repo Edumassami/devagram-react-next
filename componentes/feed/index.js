01ee371ec14c50103cfa3b79da1ef8263d9b836c
Edumassami <63077457+Edumassami@users.noreply.github.com>
2023-08-03 23:34:53 -0300

    Update index.js

--- componentes/feed/index.js
+++ componentes/feed/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Postagem from "./postagem"
+import Postagem from "./Postagem"
 import FeedService from "@/services/FeedService";
 
 const feedService = new FeedService();
